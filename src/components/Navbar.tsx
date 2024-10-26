95b7ec9f17b36d6c6a0e64fa5ef617717bf79f82
thantzarni77 <thantzarni83@gmail.com>
2024-10-26 20:24:04 +0630

    css_fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import Logo from "../../src/assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 z-10 h-auto w-full bg-[#131111] bg-opacity-85 p-2 text-2xl font-bold text-white shadow-white drop-shadow-xl backdrop-blur">
-      <Link to={"/"} className="mx-5 flex w-[30%] items-center">
+    <div className="fixed top-0 z-10 h-auto w-full bg-[#131111] bg-opacity-85 p-2 font-bold text-white shadow-white drop-shadow-xl backdrop-blur">
+      <Link to={"/"} className="mx-5 flex w-[80%] items-center lg:w-[30%]">
         <img src={Logo} alt="Logo" className="w-[10%]" />
-        <div className="mx-5 cursor-pointer">Zodiac Viewer</div>
+        <div className="mx-5 cursor-pointer text-lg lg:text-xl">
+          Zodiac Viewer
+        </div>
       </Link>
     </div>
   );
